035f307132540965905a954557e3c6d7cafe5add
Daniel McSheehy <dsm140130@utdallas.edu>
2018-11-10 22:58:28 -0600

    Adding contract integration

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 import Abi from '../SecureStorage.json';
 import Styles from '../Styles/Home.css';
 
@@ -98,30 +100,38 @@ class Home extends React.Component {
   render() {
     return (
         <div>
-        <div className="home-outer-wrapper">
-        <TextField
-          id="outlined-name"
-          label="Encryption Key"
-          className="change"
-          value={this.state.keyValue}
-          onChange={this.handleChange}
-          margin="normal"
-          variant="outlined"
-        />
-        <div className="home-button-wrapper"> 
-            <Button variant="contained" color="primary" onClick={this.createUserAccess}>
-                Purchase Data
-            </Button>
-        </div>
-        
-        
-        </div>
-        <div className="home-second-button-wrapper"> 
-            <Button variant="contained" color="primary" onClick={this.getIPFSAddress}>
-                    Get Address
-            </Button>
-        </div>
-            {this.state.ipfsAddress}
+            <div className="home-outer-wrapper">
+            <div >
+                <Card className="home-data-card">
+                    <Typography> 
+                    Labled Cat images (Rare)
+                    </Typography>
+                    Price: 100 Wei
+                </Card>
+            </div>
+            <TextField
+            id="outlined-name"
+            label="Encryption Key"
+            className="change"
+            value={this.state.keyValue}
+            onChange={this.handleChange}
+            margin="normal"
+            variant="outlined"
+            />
+            <div className="home-button-wrapper"> 
+                <Button variant="contained" color="primary" onClick={this.createUserAccess}>
+                    Purchase Data
+                </Button>
+            </div>
+            
+            
+            </div>
+            <div className="home-second-button-wrapper"> 
+                <Button variant="contained" color="primary" onClick={this.getIPFSAddress}>
+                        Get Address
+                </Button>
+            </div>
+                {this.state.ipfsAddress}
       </div>
     );
   }
