ff8c8ea765950c8a8aa1a1ce36038d0764c15b5d
Samuel Zapote <samuelzapote@outlook.com>
2018-11-10 20:08:45 -0600

    Trying out this new branch

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,44 +2,49 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Styles from '../Styles/Home.css';
-class Home extends React.Component {
-  constructor(prop) {
-    super(prop);
-    this.state = {
-        key: ''
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
 
-  handleClick() {
-   //this.setState({ togglePaymentIntput: !this.state.togglePaymentIntput });
-  }
+import indexRoutes from "routes/index.jsx";
 
-  handleChange() {
+// class Home extends React.Component {
+//   constructor(prop) {
+//     super(prop);
+//     this.state = {
+//         key: ''
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//     this.handleChange = this.handleChange.bind(this);
+//   }
 
-  }
+//   handleClick() {
+//    //this.setState({ togglePaymentIntput: !this.state.togglePaymentIntput });
+//   }
 
-  render() {
-    return (
-      <div className="home-outer-wrapper">
-        <TextField
-          id="outlined-name"
-          label="Encryption Key"
-          className="change"
-          value={this.state.key}
-          onChange={this.handleChange()}
-          margin="normal"
-          variant="outlined"
-        />
-        <div className="home-button-wrapper"> 
-            <Button variant="contained" color="primary" >
-                Primary
-            </Button>
-        </div>
-      </div>
-    );
-  }
-}
+//   handleChange() {
+
+//   }
+
+//   render() {
+//     return (
+//       <div className="home-outer-wrapper">
+//         <TextField
+//           id="outlined-name"
+//           label="Encryption Key"
+//           className="change"
+//           value={this.state.key}
+//           onChange={this.handleChange()}
+//           margin="normal"
+//           variant="outlined"
+//         />
+//         <div className="home-button-wrapper"> 
+//             <Button variant="contained" color="primary" >
+//                 Primary
+//             </Button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+import indexRoutes from "./routes/index.jsx";
+//import './index.css';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.path} key={key} component={prop.component} />;
+        })}
+      </Switch>
+    </Router>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
